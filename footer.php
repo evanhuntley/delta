ce45c5e522486847be114d0b2636c6e2bc94138d
Evan Huntley <evan.huntley@gmail.com>
2016-06-29 17:20:43 -0400

    Prepping grid and styles for footer

--- footer.php
+++ footer.php
@@ -1,6 +1,28 @@
 <footer role="contentinfo">
 	<div class="container">
-		<p>&copy;<?php echo date("Y"); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
+		<div class="footer-details">
+			<div class="org-info">
+				<?php echo ot_get_option('footer_about'); ?>
+				<ul class="social">
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				</ul>
+			</div>
+			<div class="footer-nav">
+				<?php
+	                $args = array(
+						'menu' => 'Main Nav',
+	                    'container' => 'false',
+	                    'items_wrap' => '<ul>%3$s</ul>',
+	                    );
+	                wp_nav_menu($args);
+	            ?>
+			</div>
+			<div class="org-info">
+				Test
+			</div>
+		</div>
 	</div>
 </footer>
 
